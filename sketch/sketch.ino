dd0b4d4b7d163e4d7ad515764692ea2caf3ff2f4
Zakhariy <kzm.pianist@gmail.com>
2020-08-13 03:46:33 +0300

    Second commit

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,69 +1,70 @@
 #include <avr/sleep.h>
 
-
 class AmplifierState
 {
-  private:
-  bool isAmplifierOn;
-  bool isNowPlaying;
-  unsigned long lastIdle; // Preserves the millis() read when idle was detected
-  unsigned long idleTimeToTurnOff; // The time to wait to shut down amplifier
+	private:
+	bool isAmplifierOn;
+	bool isNowPlaying;
+	unsigned long lastIdle; // Preserves the millis() read when idle was detected
+	unsigned long idleTimeToTurnOff; // The time to wait to shut down amplifier
 
-  public:
-  void init(unsigned long max_idle);
-  bool isOn() { return isAmplifierOn; }
-  bool isNotPlaying() { return isNowPlaying; }
-  bool isIdle() { return (isNowPlaying == false) && isAmplifierOn; }
-  void setSample(bool mp);
+	public:
+	void Init(unsigned long max_idle);
+	bool IsAmplifierOn() { return isAmplifierOn; }
+	bool IsNowPlaying() { return isNowPlaying; }
+	bool IsIdle() { return (isNowPlaying == false) && isAmplifierOn; }
+	void SetAmplifierSample(bool mp);
 };
 
+
 // Initializes the class, max_idle is in minutes
-void AmplifierState::init(unsigned long max_idle)
+void AmplifierState::Init(unsigned long max_idle)
 {
-  idleTimeToTurnOff = max_idle;
-  Serial.print("\t Idle time: "); Serial.println(idleTimeToTurnOff, DEC);
+	idleTimeToTurnOff = max_idle;
+    Serial.print("\t Idle time: "); Serial.println(idleTimeToTurnOff, DEC);
 }
 
 // Should be called on every sample of the audio signal. mp is true if music detected
-void AmplifierState::setSample(bool mp)
+void AmplifierState::SetAmplifierSample(bool mp)
 {
-  unsigned long now = millis(); // Take current time
-  unsigned long diff;
+	unsigned long now = millis(); // Take current time
+	unsigned long diff;
 
-  if (mp)
-  {
-    // Music is playing, act accordingly
-    isAmplifierOn = true;
-    isNowPlaying = true;
-    lastIdle = 0;
-  }
-  else
-  {
-    // No music is playing
-    if (isAmplifierOn)
-    {
-      isNowPlaying = false;
-      if (lastIdle != 0)
-      {
-        diff = 0;
-        if (now < lastIdle) // Overflow of millis()
-          diff = now + (0xFFFFFFFFL - lastIdle);
-        else
-          diff = now - lastIdle;
-        Serial.print("Idle for ");
-        Serial.println(diff, DEC);
-        if (diff > idleTimeToTurnOff)
-        {
-          // Turn off the Amplifier
-          isAmplifierOn = false;
-          Serial.println(">>> Amplifier turned off <<<");
-        }
-      }
-      else
-        lastIdle = now; // Now is first time we see idle, save the time.
-    }
-  }
+	if (mp)
+	{
+		// Music is playing, act accordingly
+		isAmplifierOn = true;
+		isNowPlaying = true;
+		lastIdle = 0;
+	}
+	else
+	{
+		// No music is playing
+		if (isAmplifierOn)
+		{
+			isNowPlaying = false;
+			if (lastIdle != 0)
+			{
+				diff = 0;
+				if (now < lastIdle) // Overflow of millis()
+					diff = now + (0xFFFFFFFFL - lastIdle);
+				else
+					diff = now - lastIdle;
+				Serial.print("Idle for ");
+				Serial.println(diff, DEC);
+				if (diff > idleTimeToTurnOff)
+				{
+					// Turn off the Amplifier
+					isAmplifierOn = false;
+					Serial.println(">>> Amplifier turned off <<<");
+				}
+			}
+			else
+				lastIdle = now; // Now is first time we see idle, save the time.
+		}
+	}
 }
+
 
 /* ===============================================================
  *  Global Variables
@@ -88,84 +89,84 @@ const int amplifierOff = 10;
 const int leftInput = A0;
 const int rightInput = A1;
 
-AmplifierState Amplifier; // And the AmplifierState class
+AmplifierState NovaAmplifier;	// And the AmplifierState class
 #define ANALOG_SAMPLES 20
-const int analogThreshold = 2; // Less than this is noise
+const int analogThreashold = 2; // Less than this is noise
 
-void updateStatus()
+void DisplayStatus()
 {
-  Serial.print("System Status:\n\tPlaying\t= ");
-  Serial.print(Amplifier.isNotPlaying(), DEC);
-  Serial.print("\n\tIdle\t= ");
-  Serial.print(Amplifier.isIdle(), DEC);
-  Serial.print("\n\tAmp. \t= ");
-  Serial.print(Amplifier.isOn(), DEC);
-  Serial.println("");
+	Serial.print("System Status:\n\tPlaying\t= ");
+	Serial.print(NovaAmplifier.IsNowPlaying(), DEC);
+	Serial.print("\n\tIdle\t= ");
+	Serial.print(NovaAmplifier.IsIdle(), DEC);
+	Serial.print("\n\tAmp. \t= ");
+	Serial.print(NovaAmplifier.IsAmplifierOn(), DEC);
+	Serial.println("");
 
-  digitalWrite(playingPin, Amplifier.isNotPlaying());
-  digitalWrite(waitingPin, Amplifier.isIdle());
-  digitalWrite(amplifierOff, Amplifier.isOn());
+	digitalWrite(playingPin, NovaAmplifier.IsNowPlaying());
+	digitalWrite(waitingPin, NovaAmplifier.IsIdle());
+	digitalWrite(amplifierOff, NovaAmplifier.IsAmplifierOn());
 }
 
 
-bool isMusicPlaying()
+bool IsMusicPlaying()
 {
-  int maxRead = 0;
-  int samples[ANALOG_SAMPLES];
-  int i;
+	int maxRead = 0;
+	int samples[ANALOG_SAMPLES];
+	int i;
 
-  Serial.print("Reading audio information:  Left ");
-  digitalWrite(powerPin, LOW);
+	Serial.print("Reading audio information:  Left ");
+	digitalWrite(powerPin, LOW);
 
-  // Take samples on right channel
-  for(i=0; i < (sizeof(samples)/sizeof(samples[0])); i++)
-  {
-    samples[i] = analogRead(rightInput);
-    delay(2 * i);
-  }
-  for(i=0; i < (sizeof(samples)/sizeof(samples[0])); i++)
-    if (samples[i] > maxRead)
-      maxRead = samples[i];
+	// Take samples on right channel
+	for(i=0; i < (sizeof(samples)/sizeof(samples[0])); i++)
+	{
+		samples[i] = analogRead(leftInput);
+		delay(2 * i);
+	}
+	for(i=0; i < (sizeof(samples)/sizeof(samples[0])); i++)
+		if (samples[i] > maxRead)
+			maxRead = samples[i];
 
-  Serial.print(maxRead, DEC);
-  Serial.print("\tTotal ");
+	Serial.print(maxRead, DEC);
+	Serial.print("\tTotal ");
 
-  // Take samples on left channel
-  for(i=0; i < (sizeof(samples)/sizeof(samples[0])); i++)
-  {
-    samples[i] = analogRead(leftInput);
-    delay(2 * i);
-  }
-  for(i=0; i < (sizeof(samples)/sizeof(samples[0])); i++)
-    if (samples[i] > maxRead)
-      maxRead = samples[i];
+	// Take samples on left channel
+	for(i=0; i < (sizeof(samples)/sizeof(samples[0])); i++)
+	{
+		samples[i] = analogRead(leftInput);
+		delay(2 * i);
+	}
+	for(i=0; i < (sizeof(samples)/sizeof(samples[0])); i++)
+		if (samples[i] > maxRead)
+			maxRead = samples[i];
 
-  digitalWrite(powerPin, HIGH);
-  Serial.println(maxRead, DEC);
+	digitalWrite(powerPin, HIGH);
+	Serial.println(maxRead, DEC);
 
-  return (maxRead > analogThreshold);
+	return ((maxRead*2) > analogThreashold);
 }
 
 
 void setup()
 {
-  Serial.begin(9600); // You can use faster if you like...
+	Serial.begin(9600); // You can use faster if you like...
 
-  // Setup the LEDs status display
-  pinMode(powerPin, OUTPUT);
-  pinMode(playingPin, OUTPUT);
-  pinMode(waitingPin, OUTPUT);
-  pinMode(amplifierOff, OUTPUT);
+	// Setup the LEDs status display
+	pinMode(powerPin, OUTPUT);
+	pinMode(playingPin, OUTPUT);
+	pinMode(waitingPin, OUTPUT);
+	pinMode(amplifierOff, OUTPUT);
 
-  Amplifier.init((unsigned long)(maxIdleMinutes * 60L * 1000L));
-  digitalWrite(powerPin, HIGH);
+	NovaAmplifier.Init((unsigned long)(maxIdleMinutes * 60L * 1000L));
+	digitalWrite(powerPin, HIGH);
 }
 
 
 
 void loop()
 {
-  Amplifier.setSample(isMusicPlaying());
-  updateStatus();
-  delay(sampleSleepTime);
+	NovaAmplifier.SetAmplifierSample(IsMusicPlaying());
+	DisplayStatus();
+	delay(sampleSleepTime);
 }
